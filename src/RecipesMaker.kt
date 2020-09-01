6ffda8eef5536b58bc156b2fab3ee82ed46ea7a4
Mariano Monzon <arielmariano.monzon2002@gmail.com>
2020-09-01 15:01:42 -0300

    Añadi todos los archivos a GitHub

---
+++ src/RecipesMaker.kt
@@ -0,0 +1,27 @@
+fun main() {
+
+    val menu = """
+    Selecciona la opción deseada
+        1. Hacer una receta
+        2. Ver mis recetas
+        3. Salir
+    """.trimIndent()
+    println(menu)
+    do {
+    val response:String? = readLine()
+        if (response.equals("1")){
+            println("Hacer una receta")
+        }else if (response.equals("2")){
+            println("Ver mis recetas")
+        }else if (response.equals("3")){
+            println("Salir")
+        }else{
+            println("Debe seleccionar una de las opciones dadas")
+        }
+
+    } while (response != "3")
+
+
+
+
+}
